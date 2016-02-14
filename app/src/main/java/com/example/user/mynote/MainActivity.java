77be5b55e1781dd8fa9b8410414c2af0d04716db
Lee <xbc1207a@gmail.com>
2016-02-14 16:42:34 +0800

    create the database

--- app/src/main/java/com/example/user/mynote/MainActivity.java
+++ app/src/main/java/com/example/user/mynote/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.user.mynote;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private NotesDAO notedb=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +16,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        notedb=new NotesDAO(this);
     }
 
     @Override

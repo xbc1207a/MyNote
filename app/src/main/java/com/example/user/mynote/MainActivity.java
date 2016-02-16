009c4f61aca3a1c7567a65b304c9d980efd9b211
Lee <xbc1207a@gmail.com>
2016-02-16 11:50:02 +0800

    add a method method : setListView

--- app/src/main/java/com/example/user/mynote/MainActivity.java
+++ app/src/main/java/com/example/user/mynote/MainActivity.java
@@ -8,10 +8,11 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private NotesDAO notedb=null;
     private ListView list=null;
-    private ArrayAdapter data=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +53,27 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onResume() {
+        setListView();
         super.onResume();
     }
     public void findView(){
         list=(ListView)findViewById(R.id.noteList);
+    }
+    public void setListView(){
+        ArrayList<Note> notes=notedb.getAllNotes();
+        ArrayAdapter<String> adapter=null;
+        ArrayList<String> strings=new ArrayList<>();
+
+        if( notes.size()==0 ){
+            String s="Nothing";
+            strings.add(s);
+        }
+        else{
+            for(int i=0;i<notes.size();i++){
+                strings.add( notes.get(i).getTitle() );
+            }
+        }
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,strings);
+        list.setAdapter( adapter );
     }
 }

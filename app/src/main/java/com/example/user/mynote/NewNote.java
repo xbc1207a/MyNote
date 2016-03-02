cabc902aa3a6066370f19dc523f3b635fb30c00c
Lee <xbc1207a@gmail.com>
2016-03-02 19:26:53 +0800

    add a datePicker

--- app/src/main/java/com/example/user/mynote/NewNote.java
+++ app/src/main/java/com/example/user/mynote/NewNote.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class NewNote extends AppCompatActivity {
     private NotesDAO noteDatabase=null;
     private EditText title=null;
     private EditText text=null;
+    private DatePicker date=null;
+    private TimePicker time=null;
+    private int year=0,month=0,day=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,26 @@ public class NewNote extends AppCompatActivity {
 
         noteDatabase=new NotesDAO(this);
         this.findView();
+        this.setContent();
     }
     public void findView(){
         title=(EditText)findViewById(R.id.modifyTitle);
         text=(EditText)findViewById(R.id.text);
+        date=(DatePicker)findViewById(R.id.datePicker);
+        time=(TimePicker)findViewById(R.id.timePicker);
+    }
+    public void setContent(){
+        Calendar today=Calendar.getInstance();
+
+        date.init( today.get(Calendar.YEAR),today.get(Calendar.MONTH),today.get(Calendar.DAY_OF_MONTH),
+                new DatePicker.OnDateChangedListener(){
+                    @Override
+                public void onDateChanged( DatePicker view,int getYear,int getMonth,int getDay ){
+                        year=getYear;
+                        month=getMonth;
+                        day=getDay;
+                    }
+        } );
     }
     public void onClick(View event){
         switch ( event.getId() ){
